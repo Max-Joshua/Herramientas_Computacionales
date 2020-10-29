05a37abcd5cb022db4d2bc057ea886389d002868
MrDrHax <alexplays003@gmail.com>
2020-10-29 11:37:27 -0600

    added velocity vectors

--- game/main.py
+++ game/main.py
@@ -27,31 +27,77 @@ class imagesLoader:
 class object:
     x = 0
     y = 0
-    sprite = pyglet.sprite.Sprite
-    def __init__(self, x : float, y : float, sprite : pyglet.sprite.Sprite):
+    xVel = 0
+    yVel = 0
+    xAxcel = 0
+    yAxcel = 0
+    drag = 10
+    def __init__( self, spriteImg, x : float = 0, y : float = 0):
         self.x = x
         self.y = y
-        self.sprite = sprite
+        self.sprite = spriteImg
+
+    def calculateNextPoint(self):
+        self.xVel += self.xAxcel * physiscDeltaTime - self.xVel / self.drag
+        self.yVel += self.yAxcel * physiscDeltaTime - self.yVel / self.drag
+        self.x += self.xVel * physiscDeltaTime
+        self.y += self.yVel * physiscDeltaTime
+        self.sprite.x = self.x
+        self.sprite.y = self.y
+
+class staticObject:
+    pass
 
 images = imagesLoader()
+
+objects = []
+
+objects.append(object(images.spriteDict['test']))
 
 @window.event
 def on_draw():
     window.clear()
-    images.spriteDict['test'].draw()
+    for object in objects:
+        object.sprite.draw()
+
+moveForce = 500
 
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == 97: # if pressed the A key
-        print("a is pressed!")
-
+        objects[0].xAxcel -= moveForce
     if symbol == 119: # if pressed the W key
         print("W is pressed!")
 
     if symbol == 100: # if pressed the D key
-        print("D is pressed!")
+        objects[0].xAxcel += moveForce
 
     if symbol == 115: # if pressed the S key
         print("S is pressed!")
+
+@window.event
+def on_key_release(symbol, modifiers):
+    if symbol == 97: # if pressed the A key
+        objects[0].xAxcel = 0
+    if symbol == 119: # if pressed the W key
+        print("W is pressed!")
+
+    if symbol == 100: # if pressed the D key
+        objects[0].xAxcel = 0
+
+    if symbol == 115: # if pressed the S key
+        print("S is pressed!")
+
+gravity = 5
+physiscDeltaTime = 0.02
+
+def runPhysics(dx):
+    for object in objects:
+        '''
+        object.yAxcel -= gravity
+        '''
+        object.calculateNextPoint()
+
+pyglet.clock.schedule_interval(runPhysics, physiscDeltaTime)
 
 pyglet.app.run()
