c0487408ed3493a79888ca197a4594e34983b684
MrDrHax <alexplays003@gmail.com>
2020-10-29 16:41:52 -0600

    COLLISSIONS!!!!!!

--- game/main.py
+++ game/main.py
@@ -12,8 +12,6 @@ objects = []
 
 plataforms = []
 
-canJump = True
-
 def gameStarter():
     with open('game/information.json') as file:
         info = json.load(file)
@@ -21,7 +19,7 @@ def gameStarter():
     objects.append(object(images.imgDict['Protagonist'], mainBatch, info['player']['x'], info['player']['y']))
 
     for plataform in info['levels'][info['player']['currentlvl']]['objects']:
-        plataforms.append(staticObject(plataform['x'], plataform['y'], plataform['width'], plataform['height'], images.imgDict['test']))
+        plataforms.append(staticObject(plataform['x'], plataform['y'], plataform['width'], plataform['height'], images.imgDict['Platform']))
 
 class imagesLoader:
     spriteDict = {}
@@ -50,6 +48,8 @@ class object:
     xAxcel = 0
     yAxcel = 0
     drag = 10
+    gravityActive = True
+    canJump = False
     def __init__( self, spriteImg, batch, x : float = 0, y : float = 0):
         self.x = x
         self.y = y
@@ -64,6 +64,7 @@ class object:
         self.sprite.y = self.y
     
     def checkCollisions(self):
+        activateGravity = False
         if self.x < 0:
             self.x = 0
             self.xVel = 0
@@ -74,15 +75,48 @@ class object:
             self.xAxcel = 0
         
         if self.y < 0:
-            global canJump
             self.y = 0
             self.yVel = 0
             self.yAxcel = 0
-            canJump = True
+            activateGravity = True
+        elif self.y <= 0:
+            activateGravity = True
         elif self.y > height - self.sprite.height:
             self.y = height - self.sprite.height
             self.yVel = 0
             self.yAxcel = 0
+
+        for plataform in plataforms:
+            if plataform.x - self.sprite.width < self.x < plataform.x + plataform.width:
+                if plataform.y - self.sprite.height < self.y < plataform.y + plataform.height:
+                    # chech best way to throw the player to
+                    offsetRenderer = 5
+                    if plataform.x - self.sprite.width  + offsetRenderer <= self.x <= plataform.x + plataform.width - 20:
+                        if plataform.y + plataform.height - 2 > self.y > plataform.y:
+                            self.y = plataform.y + plataform.height - 1
+                            self.yAxcel = 0
+                            self.yVel = 0
+                            activateGravity = True
+                        
+                        if plataform.y + plataform.height >= self.y >= plataform.y:
+                            activateGravity = True
+
+                    if plataform.y - self.sprite.height  + offsetRenderer <= self.y <= plataform.y + plataform.height - offsetRenderer:
+                        activateGravity = True
+                        if self.x + self.sprite.width / 2 < plataform.x + plataform.width / 2:
+                            self.x -= offsetRenderer - 1
+                            self.xVel = 0
+                            self.xAxcel = 0
+                        else:
+                            self.x += offsetRenderer - 1
+                            self.xVel = 0
+                            self.xAxcel = 0
+
+        if activateGravity:
+            self.canJump = True
+            self.gravityActive = False
+        else:
+            self.gravityActive = True
 
 class staticObject:
     x = 0
@@ -110,13 +144,13 @@ moveForce = 1000
 
 @window.event
 def on_key_press(symbol, modifiers):
-    global canJump
     if symbol == 97: # if pressed the A key
         objects[0].xAxcel -= moveForce
     if symbol == 119: # if pressed the W key
-        if canJump:
-            objects[0].yAxcel = 3000
-            canJump = False
+        if objects[0].canJump:
+            objects[0].yAxcel = 2000
+            objects[0].yVel = 500
+            objects[0].canJump = False
 
     if symbol == 100: # if pressed the D key
         objects[0].xAxcel += moveForce
@@ -140,12 +174,14 @@ maxGravity = -10000
 physiscDeltaTime = 0.02
 
 def runPhysics(dx):
+    
     for object in objects:
-        if object.yAxcel > maxGravity:
-            object.yAxcel -= gravity
-        
-        object.calculateNextPoint()
+        if object.gravityActive:
+            if object.yAxcel > maxGravity:
+                object.yAxcel -= gravity
+
         object.checkCollisions()
+        object.calculateNextPoint()
 
 images = imagesLoader()
 gameStarter()
