6d70778fdf5fdfdf5f06e3b25050879c020da3b1
MrDrHax <alexplays003@gmail.com>
2020-10-29 12:17:14 -0600

    stable realise of movement implementacion

--- game/main.py
+++ game/main.py
@@ -21,7 +21,7 @@ class imagesLoader:
 
         for file in files:
             self.imgDict[file[:-4]] = pyglet.image.load('game/img/' + file)
-            self.spriteDict[file[:-4]] = pyglet.sprite.Sprite(img = self.imgDict[file[:-4]])
+            #self.spriteDict[file[:-4]] = pyglet.sprite.Sprite(img = self.imgDict[file[:-4]], batch = mainBatch)
         print('images loaded')
 
 class object:
@@ -32,10 +32,10 @@ class object:
     xAxcel = 0
     yAxcel = 0
     drag = 10
-    def __init__( self, spriteImg, x : float = 0, y : float = 0):
+    def __init__( self, spriteImg, batch, x : float = 0, y : float = 0):
         self.x = x
         self.y = y
-        self.sprite = spriteImg
+        self.sprite = pyglet.sprite.Sprite(img = spriteImg, batch = batch)
 
     def calculateNextPoint(self):
         self.xVel += self.xAxcel * physiscDeltaTime - self.xVel / self.drag
@@ -44,15 +44,40 @@ class object:
         self.y += self.yVel * physiscDeltaTime
         self.sprite.x = self.x
         self.sprite.y = self.y
+    
+    def checkCollisions(self):
+        if self.x < 0:
+            self.x = 0
+            self.xVel = 0
+            self.xAxcel = 0
+        elif self.x > width - self.sprite.width:
+            self.x = width - self.sprite.width
+            self.xVel = 0
+            self.xAxcel = 0
+        
+        if self.y < 0:
+            global canJump
+            self.y = 0
+            self.yVel = 0
+            self.yAxcel = 0
+            canJump = True
+        elif self.y > height - self.sprite.height:
+            self.y = height - self.sprite.height
+            self.yVel = 0
+            self.yAxcel = 0
 
 class staticObject:
     pass
 
 images = imagesLoader()
 
+mainBatch = pyglet.graphics.Batch()
+
 objects = []
 
-objects.append(object(images.spriteDict['test']))
+objects.append(object(images.imgDict['Protagonist'], mainBatch))
+
+canJump = True
 
 @window.event
 def on_draw():
@@ -60,14 +85,17 @@ def on_draw():
     for object in objects:
         object.sprite.draw()
 
-moveForce = 500
+moveForce = 1000
 
 @window.event
 def on_key_press(symbol, modifiers):
+    global canJump
     if symbol == 97: # if pressed the A key
         objects[0].xAxcel -= moveForce
     if symbol == 119: # if pressed the W key
-        print("W is pressed!")
+        if canJump:
+            objects[0].yAxcel = 3000
+            canJump = False
 
     if symbol == 100: # if pressed the D key
         objects[0].xAxcel += moveForce
@@ -79,24 +107,21 @@ def on_key_press(symbol, modifiers):
 def on_key_release(symbol, modifiers):
     if symbol == 97: # if pressed the A key
         objects[0].xAxcel = 0
-    if symbol == 119: # if pressed the W key
-        print("W is pressed!")
 
     if symbol == 100: # if pressed the D key
         objects[0].xAxcel = 0
 
-    if symbol == 115: # if pressed the S key
-        print("S is pressed!")
-
-gravity = 5
+gravity = 100
+maxGravity = -10000
 physiscDeltaTime = 0.02
 
 def runPhysics(dx):
     for object in objects:
-        '''
-        object.yAxcel -= gravity
-        '''
+        if object.yAxcel > maxGravity:
+            object.yAxcel -= gravity
+        
         object.calculateNextPoint()
+        object.checkCollisions()
 
 pyglet.clock.schedule_interval(runPhysics, physiscDeltaTime)
 
